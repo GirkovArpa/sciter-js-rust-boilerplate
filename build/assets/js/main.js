b559969113c86e525e2c1d4f6583d8689f3995e2
GirkovArpa <girkovarpa@protonmail.com>
2021-02-25 19:34:34 -0600

    :bug: async sum function segmentation fault

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -4,13 +4,13 @@ adjustWindow();
 
 function adjustWindow() {
   // https://github.com/c-smile/sciter-js-sdk/discussions/39#discussioncomment-377697
-  const [ _, w ] = document.state.contentWidths();
+  const [_, w] = document.state.contentWidths();
   const h = document.state.contentHeight(w);
-  const [ sw, sh ] = Window.this.screenBox('frame', 'dimension');
+  const [sw, sh] = Window.this.screenBox('frame', 'dimension');
   Window.this.move((sw - w) / 2, (sh - h) / 2, w, h, true);
 }
 
-document.set_title = function set_title(text) {
+function set_title(text) {
   $('h1').textContent = Window.this.xcall('capitalize', text);
 }
 
@@ -22,11 +22,16 @@ $('#sum').on('click', () => {
   Window.this.modal(<info>{sum}</info>);
 });
 
-$('#sum_async').on('click', () => {
+$('#sum_async').on('click', async () => {
   const textboxes = $$('.sum_async');
   const numbers = textboxes.map((textbox) => textbox.value);
   const [a, b] = numbers;
-  Window.this.xcall('sum_async', a, b, function (sum) {
-    Window.this.modal(<info>{sum}</info>);
-  });
+  const sum = await sum_async(a, b);
+  Window.this.modal(<info>{sum}</info>);
 });
+
+function sum_async(a, b) {
+  return new Promise((resolve) => {
+    Window.this.xcall('sum_async', a, b, resolve);
+  });
+}
